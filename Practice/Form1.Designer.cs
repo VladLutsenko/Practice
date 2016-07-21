00a2589bb4df8e959528748067c6e10a01e26557
Vlad Lutsenko <Vlad_lutsenko@mail.ru>
2016-07-21 12:36:25 +0300

    first commit

---
+++ Practice/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Получить список процессов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(30, 96);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(332, 264);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Снять выделенный процесс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 408);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Practice/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var processList = Process.GetProcesses();
+            foreach (var proc in processList)
+                listBox1.Items.Add(String.Format($"{proc.ProcessName}.exe"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex != -1)
+            {
+                var taskName = listBox1.Items[listBox1.SelectedIndex].ToString();
+                taskName = taskName.Substring(0, taskName.Length - 4);
+                foreach (var procToKill in Process.GetProcessesByName(taskName))
+                    procToKill.Kill();
+                button1.PerformClick();
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            button2.PerformClick();
+            //BrowsApp(listBox1.Items[listBox1.SelectedIndex].ToString());
+        }
+
+        //public void BrowsApp(string nameApp)
+        //{
+        //    WindowsIdentity identity = WindowsIdentity.GetCurrent();
+        //    if (identity != null)
+        //    {
+        //        string currentUser = identity.Name.Split('\\')[1];
+
+        //        string query = "Select * from Win32_Process Where Name = \"" + nameApp + "\"";
+        //        var searcher = new ManagementObjectSearcher(query);
+        //        var processes = searcher.Get();
+
+        //        foreach (ManagementObject proc in processes)
+        //        {
+        //            string owner;
+        //            var argList = new[] { string.Empty };
+        //            int returnVal = Convert.ToInt32(proc.InvokeMethod("GetOwner", argList));
+        //            if (returnVal == 0)
+        //                owner = argList[0];
+        //            else
+        //                continue;
+
+        //            if (owner != currentUser)
+        //                continue;
+
+        //            MessageBox.Show(proc["ExecutablePath"].ToString());
+        //        }
+        //    }
+        //}
+    }
+}
